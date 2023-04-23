7df6efa64fb61560242ce442c392cc50c223a217
ricardo55991 <ricardo.barros.jr@gmail.com>
2023-04-23 05:22:41 -0300

    Validação dos tempos preenchidos

--- startup.py
+++ startup.py
@@ -3,23 +3,45 @@ import tkinter as tk
 from tkinter import filedialog
 from moviepy.editor import VideoFileClip
 from termcolor import colored
+import datetime
+import time
 #from tqdm import tqdm
 
 def main():
     os.system('cls') # Limpa o console
-    print("Escolha o arquivo de vídeo:")
+    input(colored("Pressione qualquer tecla para começar, logo após escolha o arquivo de vídeo desejado.", 'cyan', attrs=['bold'])) # Precisa ser comentado em testes
     file_path = select_file()
     name_file = os.path.basename(file_path)
     print(f"Arquivo escolhido: {colored(name_file, 'cyan')}")
 
+    # Obtem a duração do vídeo
+    clip = VideoFileClip(file_path)
+    duration = clip.duration
+    clip.close()
+
     # Obtem o tempo inicial e final do usuário
-    start_time = input(f"\nDigite o tempo inicial no formato HH:MM:SS. \n{colored('Obs: Se você deseja selecionar desde o início do vídeo pressione a tecla Enter.', 'grey')} \nTempo inicial: ")
-    if start_time == "": 
-        start_time = "00:00:00"
-    end_time = input("Digite o tempo final no formato HH:MM:SS: ")
+    while True:
+        start_time = input(f"\nDigite o tempo inicial no formato HH:MM:SS. \n{colored('Obs: Se você deseja selecionar desde o início do vídeo pressione a tecla Enter.', 'grey')} \nTempo inicial: ")
+        if start_time == "": 
+            start_time = "00:00:00"
+        end_time = input("Digite o tempo final no formato HH:MM:SS: ")
+        if valid_time(start_time, end_time, duration):
+            break
 
     resolution = resolutions(file_path)
     process_video(file_path, start_time, end_time, resolution, name_file)
+
+    while True:
+        print(colored("\nDeseja repetir o procedimento com outro vídeo?", 'cyan', attrs=['bold']))
+        print(colored("1 - Sim\n2 - Não", 'yellow', attrs=['bold']))
+        repeat = input("Opção: ")
+        if repeat == "1":
+            main()
+        elif repeat == "2":
+            break
+        else:
+            print(colored("Opção inválida!", 'red'))
+            time.sleep(2)
 
 # Obtem o caminho do arquivo de vídeo
 def select_file():
@@ -82,6 +104,28 @@ def process_video(file_path, start_time, end_time, resolution, name_file):
     
     clip_resized.write_videofile(os.path.join("videos_recortados", f"{name_file}"), audio=True)
     print(colored("\nO vídeo foi recortado com sucesso!", 'green', attrs=['bold']))
+
+
+# Valida os tempos inicias e finais
+def valid_time(start_time, end_time, total_time):
+    # Converte tempo para segundos
+    start_sec = datetime.datetime.strptime(start_time, '%H:%M:%S').time().second + datetime.datetime.strptime(start_time, '%H:%M:%S').time().minute*60 + datetime.datetime.strptime(start_time, '%H:%M:%S').time().hour*3600
+    end_sec = datetime.datetime.strptime(end_time, '%H:%M:%S').time().second + datetime.datetime.strptime(end_time, '%H:%M:%S').time().minute*60 + datetime.datetime.strptime(end_time, '%H:%M:%S').time().hour*3600
+
+    if end_sec > total_time:
+        print(colored("Erro: O tempo final é maior que a duração do vídeo", "red", attrs=['bold']))
+        return False
+    elif start_sec > total_time:
+        print(colored("Erro: O tempo inicial é maior que a duração do vídeo", "red", attrs=['bold']))
+        return False
+    elif start_sec > end_sec:
+        print(colored("Erro: O tempo inicial é maior que o tempo final", "red", attrs=['bold']))
+        return False
+    elif end_sec < start_sec:
+        print(colored("Erro: O tempo final é menor que o tempo inicial", "red", attrs=['bold']))
+        return False
+    else:
+        return True
 
 
 # Start
